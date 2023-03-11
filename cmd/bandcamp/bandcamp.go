9eb65741e0e16bf2cffab1ca097f2cc5b69452cc
Steven Penny <rqwwof@riseup.net>
2023-03-11 07:28:48 -0600

    new file:   cmd/bandcamp/bandcamp.go

---
+++ cmd/bandcamp/bandcamp.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+   "2a.pages.dev/mech/bandcamp"
+   "2a.pages.dev/rosso/http"
+   "encoding/json"
+   "io"
+   "os"
+   "time"
+)
+
+func (f flags) tralbum(tralb *bandcamp.Tralbum) error {
+   for _, track := range tralb.Tracks {
+      if f.info {
+         enc := json.NewEncoder(os.Stdout)
+         enc.SetEscapeHTML(false)
+         enc.SetIndent("", " ")
+         err := enc.Encode(track)
+         if err != nil {
+            return err
+         }
+      } else if track.Streaming_URL != nil {
+         res, err := bandcamp.Client.Get(track.Streaming_URL.MP3_128)
+         if err != nil {
+            return err
+         }
+         file, err := os.Create(track.Name() + ".mp3")
+         if err != nil {
+            return err
+         }
+         pro := http.Progress_Bytes(file, res.ContentLength)
+         if _, err := io.Copy(pro, res.Body); err != nil {
+            return err
+         }
+         if err := res.Body.Close(); err != nil {
+            return err
+         }
+         if err := file.Close(); err != nil {
+            return err
+         }
+         time.Sleep(f.sleep)
+      }
+   }
+   return nil
+}
+
+func (f flags) band(param *bandcamp.Params) error {
+   band, err := param.Band()
+   if err != nil {
+      return err
+   }
+   for _, item := range band.Discography {
+      tralb, err := item.Tralbum()
+      if err != nil {
+         return err
+      }
+      if err := f.tralbum(tralb); err != nil {
+         return err
+      }
+   }
+   return nil
+}
