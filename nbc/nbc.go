38fac9d3d9a0d03ee98462e3c01d4fec7dacc6e5
Steven Penny <rqwwof@riseup.net>
2023-03-19 16:01:01 -0500

    new file:   nbc/metadata.go

--- nbc/nbc.go
+++ nbc/nbc.go
@@ -2,15 +2,7 @@ package nbc
 
 import (
    "2a.pages.dev/rosso/http"
-   "bytes"
-   "crypto/hmac"
-   "crypto/sha256"
-   "encoding/hex"
-   "encoding/json"
-   "io"
-   "strconv"
    "strings"
-   "time"
 )
 
 const query = `
@@ -42,23 +34,7 @@ query bonanzaPage(
 }
 `
 
-var (
-   Client = http.Default_Client
-   secret_key = []byte("2b84a073ede61c766e4c0b3f1e656f7f")
-)
-
-func authorization() string {
-   now := strconv.FormatInt(time.Now().UnixMilli(), 10)
-   str := new(strings.Builder)
-   str.WriteString("NBC-Security key=android_nbcuniversal,version=2.4")
-   str.WriteString(",time=")
-   str.WriteString(now)
-   str.WriteString(",hash=")
-   mac := hmac.New(sha256.New, secret_key)
-   io.WriteString(mac, now)
-   hex.NewEncoder(str).Write(mac.Sum(nil))
-   return str.String()
-}
+var Client = http.Default_Client
 
 func graphQL_compact(s string) string {
    old_new := []string{
@@ -69,92 +45,6 @@ func graphQL_compact(s string) string {
       strings.Repeat(" ", 3), " ",
    }
    return strings.NewReplacer(old_new...).Replace(s)
-}
-
-type Metadata struct {
-   MPX_Account_ID string `json:"mpxAccountId"`
-   MPX_GUID string `json:"mpxGuid"`
-   Series_Short_Title string `json:"seriesShortTitle"`
-   Secondary_Title string `json:"secondaryTitle"`
-}
-
-func New_Metadata(guid int64) (*Metadata, error) {
-   var p page_request
-   p.Query = graphQL_compact(query)
-   p.Variables.App = "nbc"
-   p.Variables.Name = strconv.FormatInt(guid, 10)
-   p.Variables.One_App = true
-   p.Variables.Platform = "android"
-   p.Variables.Type = "VIDEO"
-   body, err := json.MarshalIndent(p, "", " ")
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://friendship.nbc.co/v2/graphql", bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("Content-Type", "application/json")
-   res, err := Client.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   var page struct {
-      Data struct {
-         Bonanza_Page struct {
-            Metadata Metadata
-         } `json:"bonanzaPage"`
-      }
-   }
-   if err := json.NewDecoder(res.Body).Decode(&page); err != nil {
-      return nil, err
-   }
-   return &page.Data.Bonanza_Page.Metadata, nil
-}
-
-func (m Metadata) Name() string {
-   var b strings.Builder
-   b.WriteString(m.Series_Short_Title)
-   b.WriteByte('-')
-   b.WriteString(m.Secondary_Title)
-   return b.String()
-}
-
-func (m Metadata) Video() (*Video, error) {
-   var v video_request
-   v.Device = "android"
-   v.Device_ID = "android"
-   v.External_Advertiser_ID = "NBC"
-   v.MPX.Account_ID = m.MPX_Account_ID
-   body, err := json.MarshalIndent(v, "", " ")
-   if err != nil {
-      return nil, err
-   }
-   var str strings.Builder
-   str.WriteString("http://access-cloudpath.media.nbcuni.com")
-   str.WriteString("/access/vod/nbcuniversal/")
-   str.WriteString(m.MPX_GUID)
-   req, err := http.NewRequest("POST", str.String(), bytes.NewReader(body))
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "Authorization": {authorization()},
-      "Content-Type": {"application/json"},
-   }
-   res, err := Client.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   vid := new(Video)
-   if err := json.NewDecoder(res.Body).Decode(vid); err != nil {
-      return nil, err
-   }
-   return vid, nil
 }
 
 type Video struct {
