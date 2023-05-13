97046bda33aa2863a412b358e82456415b9be808
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-13 16:40:19 -0500

    new file:   blog/2023-05/cbc.go

--- amc/auth_test.go
+++ amc/auth_test.go
@@ -2,19 +2,32 @@ package amc
 
 import (
    "os"
+   "strings"
    "testing"
 )
 
+func sign_in(name string) ([]string, error) {
+   data, err := os.ReadFile(name)
+   if err != nil {
+      return nil, err
+   }
+   return strings.Split(string(data), "\n"), nil
+}
+
 func Test_Login(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   account, err := sign_in(home + "/mech/amc.txt")
+   if err != nil {
+      t.Fatal(err)
+   }
    auth, err := Unauth()
    if err != nil {
       t.Fatal(err)
    }
-   if err := auth.Login(email, password); err != nil {
-      t.Fatal(err)
-   }
-   home, err := os.UserHomeDir()
-   if err != nil {
+   if err := auth.Login(account[0], account[1]); err != nil {
       t.Fatal(err)
    }
    if err := auth.Write_File(home + "/mech/amc.json"); err != nil {
---
+++ blog/2023-05/cbc_test.go
@@ -0,0 +1,36 @@
+package cbc
+
+import (
+   "fmt"
+   "os"
+   "strings"
+   "testing"
+)
+
+func Test_Profile(t *testing.T) {
+   profile()
+}
+
+func Test_Token(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   account, err := sign_in(home + "/mech/cbc.txt")
+   if err != nil {
+      t.Fatal(err)
+   }
+   tok, err := new_token(account[0], account[1])
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", tok)
+}
+
+func sign_in(name string) ([]string, error) {
+   data, err := os.ReadFile(name)
+   if err != nil {
+      return nil, err
+   }
+   return strings.Split(string(data), "\n"), nil
+}
