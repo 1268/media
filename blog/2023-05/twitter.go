9bc660d8c1bc1ffb59f161abb03d80fb755c42a0
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-20 22:55:11 -0500

    new file:   blog/2023-05/other.go

--- blog/2023-05/twitter.go
+++ blog/2023-05/twitter.go
@@ -1,109 +1,107 @@
 package twitter
 
 import (
-   "io"
-   "net/http"
-   "net/http/httputil"
+   "2a.pages.dev/rosso/http"
+   "encoding/json"
    "net/url"
-   "os"
-   "strings"
 )
 
-func token() {
-   req_body := strings.NewReader(`grant_type=client_credentials`)
-   var req http.Request
-   req.Header = make(http.Header)
-   req.Method = "POST"
-   req.URL = new(url.URL)
-   req.URL.Host = "api.twitter.com"
-   req.URL.Path = "/oauth2/token"
-   req.URL.Scheme = "https"
-   req.Body = io.NopCloser(req_body)
-   req.Header["Content-Type"] = []string{"application/x-www-form-urlencoded"}
-   req.SetBasicAuth(
-      "3nVuSoBZnx6U4vzUxf5w",
-      "Bcs59EFbbsdF6Sl9Ng71smgStWEGwXXKSjYvPVt7qys",
-   )
-   res, err := new(http.Transport).RoundTrip(&req)
-   if err != nil {
-      panic(err)
-   }
-   defer res.Body.Close()
-   res_body, err := httputil.DumpResponse(res, true)
-   if err != nil {
-      panic(err)
-   }
-   os.Stdout.Write(res_body)
+const bearer = "AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F"
+
+type Guest struct {
+   Guest_Token string
 }
-func search() {
-   req := new(http.Request)
-   req.Header = make(http.Header)
-   req.Header["Authorization"] = []string{"OAuth " + strings.Join([]string{
-      `oauth_consumer_key="3nVuSoBZnx6U4vzUxf5w"`,
-      `oauth_nonce="5775992602006906429184916339582"`,
-      `oauth_signature_method="HMAC-SHA1"`,
-      `oauth_timestamp="1684614059"`,
-      `oauth_version="1.0"`,
-      `oauth_token="1660017733327630336-FClvhvGL5hO3eahU1O2cuCBHbEwOyc"`,
-      // needs to be encoded:
-      `oauth_signature="I64WGB%2BOG0Pe87pCKzi4w091w2w%3D"`,
-   }, ",")}
-   req.URL = new(url.URL)
-   req.URL.Host = "api.twitter.com"
-   req.URL.Path = "/2/search/adaptive.json"
-   req.URL.Scheme = "https"
-   val := make(url.Values)
-   val["q"] = []string{"filter:spaces"}
-   val["ext"] = []string{"mediaRestrictions,altText,mediaStats,mediaColor,info360,highlightedLabel,hasNftAvatar,unmentionInfo,editControl,previousCounts,limitedActionResults,superFollowMetadata"}
-   val["cards_platform"] = []string{"Android-12"}
-   val["earned"] = []string{"true"}
-   val["include_blocked_by"] = []string{"true"}
-   val["include_blocking"] = []string{"true"}
-   val["include_cards"] = []string{"true"}
-   val["include_carousels"] = []string{"true"}
-   val["include_composer_source"] = []string{"true"}
-   val["include_entities"] = []string{"true"}
-   val["include_ext_birdwatch_pivot"] = []string{"true"}
-   val["include_ext_edit_control"] = []string{"true"}
-   val["include_ext_has_nft_avatar"] = []string{"true"}
-   val["include_ext_is_blue_verified"] = []string{"true", "true"}
-   val["include_ext_is_tweet_translatable"] = []string{"true", "true"}
-   val["include_ext_limited_action_results"] = []string{"true"}
-   val["include_ext_media_availability"] = []string{"true"}
-   val["include_ext_previous_counts"] = []string{"true"}
-   val["include_ext_professional"] = []string{"true"}
-   val["include_ext_profile_image_shape"] = []string{"true", "true"}
-   val["include_ext_sensitive_media_warning"] = []string{"true"}
-   val["include_ext_trusted_friends_metadata"] = []string{"true"}
-   val["include_ext_verified_type"] = []string{"true", "true"}
-   val["include_ext_views"] = []string{"true"}
-   val["include_media_features"] = []string{"true"}
-   val["include_profile_interstitial_type"] = []string{"true"}
-   val["include_quote_count"] = []string{"true"}
-   val["include_reply_count"] = []string{"true"}
-   val["include_user_entities"] = []string{"true"}
-   val["include_viewer_quick_promote_eligibility"] = []string{"true"}
-   val["query_source"] = []string{"typed_query"}
-   val["simple_quoted_tweet"] = []string{"true"}
-   val["spelling_corrections"] = []string{"true"}
-   val["tweet_mode"] = []string{"extended"}
-   val["tweet_search_mode"] = []string{"top"}
-   req.URL.RawQuery = val.Encode()
-   req.ProtoMajor = 1
-   req.ProtoMinor = 1
-   b, err := httputil.DumpRequest(req, false)
+
+func New_Guest() (*Guest, error) {
+   req := http.Post(&url.URL{
+      Scheme: "https",
+      Host: "api.twitter.com",
+      Path: "/1.1/guest/activate.json",
+   })
+   req.Header.Set("Authorization", "Bearer " + bearer)
+   res, err := http.Default_Client.Do(req)
    if err != nil {
-      panic(err)
-   }
-   os.Stdout.Write(b)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      panic(err)
+      return nil, err
    }
    defer res.Body.Close()
-   res_body, err := httputil.DumpResponse(res, true)
-   if err != nil {
-      panic(err)
+   g := new(Guest)
+   if err := json.NewDecoder(res.Body).Decode(g); err != nil {
+      return nil, err
    }
-   os.Stdout.Write(res_body)
+   return g, nil
+}
+
+func (g Guest) flow_welcome() (*task, error) {
+   req := http.Post(&url.URL{
+      Scheme: "https",
+      Host: "api.twitter.com",
+      Path: "/1.1/onboarding/task.json",
+      RawQuery: "flow_name=welcome",
+   })
+   req.Header = http.Header{
+      "Authorization": {"Bearer " + bearer},
+      "Content-Type": {"application/json"},
+      "User-Agent": {"TwitterAndroid/99"},
+      "X-Guest-Token": {g.Guest_Token},
+   }
+   var t task
+   {
+      t.Input_Flow_Data = new(flow_data)
+      t.Input_Flow_Data.Flow_Context.Start_Location.Location = "splash_screen"
+      b, err := json.MarshalIndent(t, "", " ")
+      if err != nil {
+         return nil, err
+      }
+      req.Body_Bytes(b)
+   }
+   res, err := http.Default_Client.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
+      return nil, err
+   }
+   return &t, nil
+}
+
+func (g Guest) next_link(t *task) (*http.Response, error) {
+   req := http.Post(&url.URL{
+      Scheme: "https",
+      Host: "api.twitter.com",
+      Path: "/1.1/onboarding/task.json",
+   })
+   req.Header["Authorization"] = []string{"Bearer " + bearer}
+   req.Header["Content-Type"] = []string{"application/json"}
+   req.Header["X-Guest-Token"] = []string{g.Guest_Token}
+   {
+      t.Input_Flow_Data = nil
+      b, err := json.MarshalIndent(t, "", " ")
+      if err != nil {
+         return nil, err
+      }
+      req.Body_Bytes(b)
+   }
+   return http.Default_Client.Do(req)
+}
+
+type input struct {
+   Open_Link struct {
+      Link string
+   }
+   Subtask_ID string
+}
+
+type flow_data struct {
+   Flow_Context struct {
+      Start_Location struct {
+         Location string `json:"location"`
+      } `json:"start_location"`
+   } `json:"flow_context"`
+}
+
+type task struct {
+   Flow_Token *string `json:"flow_token"`
+   Input_Flow_Data *flow_data `json:"input_flow_data,omitempty"`
+   Subtask_Inputs []input `json:",omitempty"`
 }
