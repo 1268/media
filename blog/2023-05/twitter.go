71958a54c420ca000b81b8470c2ae98ae6071f3e
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-21 10:13:44 -0500

    new file:   blog/2023-05/crypto.go

--- blog/2023-05/twitter.go
+++ blog/2023-05/twitter.go
@@ -6,6 +6,53 @@ import (
    "net/url"
 )
 
+func (x subtask) search() (*http.Response, error) {
+   val := make(url.Values)
+   val["cards_platform"] = []string{"Android-12"}
+   val["earned"] = []string{"true"}
+   val["ext"] = []string{"mediaRestrictions,altText,mediaStats,mediaColor,info360,highlightedLabel,superFollowMetadata,hasNftAvatar,unmentionInfo"}
+   val["include_blocked_by"] = []string{"true"}
+   val["include_blocking"] = []string{"true"}
+   val["include_cards"] = []string{"true"}
+   val["include_carousels"] = []string{"true"}
+   val["include_composer_source"] = []string{"true"}
+   val["include_entities"] = []string{"true"}
+   val["include_ext_enrichments"] = []string{"true"}
+   val["include_ext_has_nft_avatar"] = []string{"true"}
+   val["include_ext_media_availability"] = []string{"true"}
+   val["include_ext_professional"] = []string{"true"}
+   val["include_ext_replyvoting_downvote_perspective"] = []string{"true"}
+   val["include_ext_sensitive_media_warning"] = []string{"true"}
+   val["include_media_features"] = []string{"true"}
+   val["include_profile_interstitial_type"] = []string{"true"}
+   val["include_quote_count"] = []string{"true"}
+   val["include_reply_count"] = []string{"true"}
+   val["include_user_entities"] = []string{"true"}
+   val["include_viewer_quick_promote_eligibility"] = []string{"true"}
+   val["q"] = []string{"filter:spaces"}
+   val["query_source"] = []string{"typed_query"}
+   val["simple_quoted_tweet"] = []string{"true"}
+   val["spelling_corrections"] = []string{"true"}
+   val["tweet_mode"] = []string{"extended"}
+   val["tweet_search_mode"] = []string{"top"}
+   req := http.Get(&url.URL{
+      Scheme: "https",
+      Host: "api.twitter.com",
+      Path: "/2/search/adaptive.json",
+      RawQuery: val.Encode(),
+   })
+   auth := OAuth1{
+      AccessToken: x.Open_Account.OAuth_Token,
+      AccessSecret: x.Open_Account.OAuth_Token_Secret,
+      ConsumerKey: "3nVuSoBZnx6U4vzUxf5w",
+      ConsumerSecret: "Bcs59EFbbsdF6Sl9Ng71smgStWEGwXXKSjYvPVt7qys",
+   }
+   req.Header["Authorization"] = []string{
+      auth.BuildOAuth1Header(req.Method, req.URL.String(), val),
+   }
+   return http.Default_Client.Do(req)
+}
+
 func flow_welcome(g *guest) (*task, error) {
    req := http.Post(&url.URL{
       Scheme: "https",
