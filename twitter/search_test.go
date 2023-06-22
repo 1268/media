ebf8133bd6fb7219aac6d24f0b5c9166286fbc65
Steven Penny <srpen6+4cq2@gmail.com>
2023-06-22 16:52:35 -0500

    new file:   twitter/flow.go

---
+++ twitter/search_test.go
@@ -0,0 +1,34 @@
+package twitter
+
+import (
+   "2a.pages.dev/rosso/http"
+   "encoding/json"
+   "os"
+   "testing"
+)
+
+func Test_Search(t *testing.T) {
+   http.Default_Client.Log_Level = 2
+   access, err := access_token()
+   if err != nil {
+      t.Fatal(err)
+   }
+   guest, err := guest_token(access)
+   if err != nil {
+      t.Fatal(err)
+   }
+   f, err := welcome(access, guest)
+   if err != nil {
+      t.Fatal(err)
+   }
+   if err := f.next_link(access, guest); err != nil {
+      t.Fatal(err)
+   }
+   s, err := f.open_account().search("filter:spaces")
+   if err != nil {
+      t.Fatal(err)
+   }
+   enc := json.NewEncoder(os.Stdout)
+   enc.SetIndent("", " ")
+   enc.Encode(s)
+}
