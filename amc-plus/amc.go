aa997f8be1f23fc8362185626bf611298d1bbbd1
Steven Penny <srpen6+4cq2@gmail.com>
2023-07-04 17:58:07 -0500

    deleted:    amc-plus/content.go

--- amc-plus/amc.go
+++ amc-plus/amc.go
@@ -7,8 +7,110 @@ import (
    "net/url"
    "os"
    "strings"
+   "time"
 )
 
+func (v Video) Season() (int64, error) {
+   return v.Meta.Season, nil
+}
+
+func (v Video) Episode() (int64, error) {
+   return v.Meta.Episode_Number, nil
+}
+
+func (v Video) Series() string {
+   return v.Meta.Show_Title
+}
+
+type Video struct {
+   Meta struct {
+      Show_Title string `json:"showTitle"`
+      Season int64 `json:",string"`
+      Episode_Number int64 `json:"episodeNumber"`
+      Airdate string // 1996-01-01T00:00:00.000Z
+   }
+   Text struct {
+      Title string
+   }
+}
+
+func (v Video) Title() string {
+   return v.Text.Title
+}
+
+func (v Video) Date() (time.Time, error) {
+   return time.Parse(time.RFC3339, v.Meta.Airdate)
+}
+
+type Content struct {
+   Data	struct {
+      Children []struct {
+         Properties json.RawMessage
+         Type string
+      }
+   }
+}
+
+// This accepts full URL or path only.
+func (a Auth) Content(ref string) (*Content, error) {
+   // Shows must use `path`, and movies must use `path/watch`. If trial has
+   // expired, you will get `.data.type` of `redirect`. You can remove the
+   // `/watch` to resolve this, but the resultant response will still be
+   // missing `video-player-ap`.
+   url_path := func(r *http.Request) error {
+      p, err := url.Parse(ref)
+      if err != nil {
+         return err
+      }
+      if strings.HasPrefix(p.Path, "/movies/") {
+         r.URL.Path += "/watch"
+      }
+      r.URL.Path += p.Path
+      return nil
+   }
+   req := http.Get(&url.URL{
+      Scheme: "https",
+      Host: "gw.cds.amcn.com",
+      Path: "/content-compiler-cr/api/v1/content/amcn/amcplus/path",
+   })
+   err := url_path(req)
+   if err != nil {
+      return nil, err
+   }
+   // If you request once with headers, you can request again without any
+   // headers for 10 minutes, but then headers are required again
+   req.Header = http.Header{
+      "Authorization": {"Bearer " + a.Data.Access_Token},
+      "X-Amcn-Network": {"amcplus"},
+      "X-Amcn-Tenant": {"amcn"},
+   }
+   res, err := http.Default_Client.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   con := new(Content)
+   if err := json.NewDecoder(res.Body).Decode(con); err != nil {
+      return nil, err
+   }
+   return con, nil
+}
+
+func (c Content) Video() (*Video, error) {
+   for _, child := range c.Data.Children {
+      if child.Type == "video-player-ap" {
+         var s struct {
+            Current_Video Video `json:"currentVideo"`
+         }
+         err := json.Unmarshal(child.Properties, &s)
+         if err != nil {
+            return nil, err
+         }
+         return &s.Current_Video, nil
+      }
+   }
+   return nil, errors.New("video-player-ap not present")
+}
 func (a Auth) Playback(ref string) (*Playback, error) {
    // request body
    req_body := func(r *http.Request) error {
--- amc-plus/amc_test.go
+++ amc-plus/amc_test.go
@@ -4,10 +4,43 @@ import (
    "2a.pages.dev/mech"
    "2a.pages.dev/mech/widevine"
    "encoding/base64"
+   "fmt"
    "os"
    "testing"
+   "time"
 )
 
+func Test_Content(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   auth, err := Read_Auth(home + "/amc-plus/auth.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   for _, test := range tests {
+      con, err := auth.Content(test.address)
+      if err != nil {
+         t.Fatal(err)
+      }
+      vid, err := con.Video()
+      if err != nil {
+         t.Fatal(err)
+      }
+      name, err := mech.Name(vid)
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Println(name)
+      time.Sleep(time.Second)
+   }
+}
+
+const (
+   episode = iota
+   movie
+)
 func Test_Login(t *testing.T) {
    auth, err := Unauth()
    if err != nil {
@@ -83,5 +116,19 @@ func Test_Post(t *testing.T) {
    if keys.Content().String() != test.key {
       t.Fatal(keys)
    }
+}
+var tests = []struct {
+   address string
+   key string
+   pssh string
+} {
+   // amcplus.com/shows/orphan-black/episodes/season-1-instinct--1011152
+   episode: {
+      address: "/shows/orphan-black/episodes/season-1-instinct--1011152",
+      key: "95f11e40064f47007e7d950bd52d7b95",
+      pssh: "AAAAVnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAADYIARIQJqlCz6NjSI2kDWew20wbGRoNd2lkZXZpbmVfdGVzdCIIMTIzNDU2NzgyB2RlZmF1bHQ=",
+   },
+   // amcplus.com/movies/nocebo--1061554
+   movie: {address: "/movies/nocebo--1061554"},
 }
 
