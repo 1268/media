34c339bb0b05d20db56eb0ff1a9a9b8214fdd5c1
Steven Penny <srpen6+1268@gmail.com>
2023-07-11 21:23:49 -0500

    deleted:    youtube/image.go

--- youtube/youtube_test.go
+++ youtube/youtube_test.go
@@ -2,10 +2,31 @@ package youtube
 
 import (
    "fmt"
+   "net/http"
    "testing"
    "time"
 )
 
+const image_test = "UpNXI3_ctAc"
+
+func Test_Image(t *testing.T) {
+   req, err := http.NewRequest("HEAD", "", nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   for _, img := range Images {
+      req.URL = img.URL(image_test)
+      fmt.Println("HEAD", req.URL)
+      res, err := new(http.Transport).RoundTrip(req)
+      if err != nil {
+         t.Fatal(err)
+      }
+      if res.StatusCode != http.StatusOK {
+         t.Fatal(res.Status)
+      }
+      time.Sleep(99 * time.Millisecond)
+   }
+}
 var id_tests = []string{
    "https://youtube.com/shorts/9Vsdft81Q6w",
    "https://youtube.com/watch?v=XY-hOqcPGCY",
