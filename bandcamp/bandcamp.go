8472a472da2efdd58a2bc184542766d196693e3b
Steven Penny <srpen6+1268@gmail.com>
2023-07-15 18:12:13 -0500

    new file:   bandcamp/params.go

--- bandcamp/bandcamp.go
+++ bandcamp/bandcamp.go
@@ -1,14 +1,36 @@
 package bandcamp
 
 import (
-   "154.pages.dev/encoding/xml"
    "encoding/json"
-   "io"
    "net/http"
    "net/url"
    "strconv"
    "time"
 )
+
+func (t Track) String() string {
+   var b []byte
+   b = append(b, "track num: "...)
+   b = strconv.AppendInt(b, t.Track_Num, 10)
+   b = append(b, "\ntitle: "...)
+   b = append(b, t.Title...)
+   b = append(b, "\nband: "...)
+   b = append(b, t.Band_Name...)
+   if t.Streaming_URL != nil {
+      b = append(b, "\nURL: "...)
+      b = append(b, t.Streaming_URL.MP3_128...)
+   }
+   return string(b)
+}
+
+type Track struct {
+   Track_Num int64
+   Title string
+   Band_Name string
+   Streaming_URL *struct {
+      MP3_128 string `json:"mp3-128"`
+   }
+}
 
 func new_tralbum(typ byte, id int) (*Tralbum, error) {
    req := http.Get(&url.URL{
@@ -46,26 +68,6 @@ func (i invalid_type) Error() string {
    b = append(b, "invalid type "...)
    b = strconv.AppendQuote(b, i.value)
    return string(b)
-}
-
-type Params struct {
-   A_ID int
-   I_ID int
-   I_Type string
-}
-
-func (p Params) Band() (*Band, error) {
-   return new_band(p.A_ID)
-}
-
-func (p Params) Tralbum() (*Tralbum, error) {
-   switch p.I_Type {
-   case "a":
-      return new_tralbum('a', p.I_ID)
-   case "t":
-      return new_tralbum('t', p.I_ID)
-   }
-   return nil, invalid_type{p.I_Type}
 }
 
 const (
@@ -187,29 +189,5 @@ type Tralbum struct {
    Title string
    Tralbum_Artist string
    Tracks []Track
-}
-
-func New_Params(ref string) (*Params, error) {
-   res, err := http.Default_Client.Get(ref)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   text, err := io.ReadAll(res.Body)
-   if err != nil {
-      return nil, err
-   }
-   sep := []byte(`<p id="report-account-vm"`)
-   var p struct {
-      Report_Params []byte `xml:"data-tou-report-params,attr"`
-   }
-   if err := xml.Cut_Before(text, sep, &p); err != nil {
-      return nil, err
-   }
-   param := new(Params)
-   if err := json.Unmarshal(p.Report_Params, param); err != nil {
-      return nil, err
-   }
-   return param, nil
 }
 
--- bandcamp/bandcamp_test.go
+++ bandcamp/bandcamp_test.go
@@ -6,24 +6,6 @@ import (
    "time"
 )
 
-var tests = []string{
-   "https://schnaussandmunk.bandcamp.com",
-   "https://schnaussandmunk.bandcamp.com/album/passage-2",
-   "https://schnaussandmunk.bandcamp.com/music",
-   "https://schnaussandmunk.bandcamp.com/track/amaris-2",
-}
-
-func Test_Param(t *testing.T) {
-   for _, test := range tests {
-      param, err := New_Params(test)
-      if err != nil {
-         t.Fatal(err)
-      }
-      fmt.Printf("%+v\n", param)
-      time.Sleep(99 * time.Millisecond)
-   }
-}
-
 const art_ID = 3809045440
 
 func Test_Image(t *testing.T) {
