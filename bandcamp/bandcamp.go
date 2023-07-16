e5a9df7b142b6a91a5381ffca41cf0a672457ae5
Steven Penny <srpen6+1268@gmail.com>
2023-07-16 07:39:38 -0500

    +func (t Track) String() string {

--- bandcamp/bandcamp.go
+++ bandcamp/bandcamp.go
@@ -8,30 +8,6 @@ import (
    "time"
 )
 
-func (t Track) String() string {
-   var b []byte
-   b = append(b, "track num: "...)
-   b = strconv.AppendInt(b, t.Track_Num, 10)
-   b = append(b, "\ntitle: "...)
-   b = append(b, t.Title...)
-   b = append(b, "\nband: "...)
-   b = append(b, t.Band_Name...)
-   if t.Streaming_URL != nil {
-      b = append(b, "\nURL: "...)
-      b = append(b, t.Streaming_URL.MP3_128...)
-   }
-   return string(b)
-}
-
-type Track struct {
-   Track_Num int64
-   Title string
-   Band_Name string
-   Streaming_URL *struct {
-      MP3_128 string `json:"mp3-128"`
-   }
-}
-
 func new_tralbum(typ byte, id int) (*Tralbum, error) {
    req := http.Get(&url.URL{
       Scheme: "http",
@@ -191,3 +167,26 @@ type Tralbum struct {
    Tracks []Track
 }
 
+func (t Track) String() string {
+   var b []byte
+   b = append(b, "track num: "...)
+   b = strconv.AppendInt(b, t.Track_Num, 10)
+   b = append(b, "\ntitle: "...)
+   b = append(b, t.Title...)
+   b = append(b, "\nband: "...)
+   b = append(b, t.Band_Name...)
+   if t.Streaming_URL != nil {
+      b = append(b, "\nURL: "...)
+      b = append(b, t.Streaming_URL.MP3_128...)
+   }
+   return string(b)
+}
+
+type Track struct {
+   Track_Num int64
+   Title string
+   Band_Name string
+   Streaming_URL *struct {
+      MP3_128 string `json:"mp3-128"`
+   }
+}
