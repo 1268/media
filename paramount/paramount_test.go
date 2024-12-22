b85550d1003f967d599b3e9f37dd65983dc389ad
3052 <spen+3052@protonmail.com>
2024-12-22 12:48:41 -0600

    deleted:    blog/kanopy/14881161.json

--- paramount/paramount_test.go
+++ paramount/paramount_test.go
@@ -11,71 +11,6 @@ import (
    "time"
 )
 
-func TestWidevine(t *testing.T) {
-   home, err := os.UserHomeDir()
-   if err != nil {
-      t.Fatal(err)
-   }
-   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
-   if err != nil {
-      t.Fatal(err)
-   }
-   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
-   if err != nil {
-      t.Fatal(err)
-   }
-   var app AppToken
-   err = app.ComCbsApp()
-   if err != nil {
-      t.Fatal(err)
-   }
-   for _, test := range tests {
-      session, err := app.Session(test.content_id)
-      if err != nil {
-         t.Fatal(err)
-      }
-      var pssh widevine.PsshData
-      pssh.ContentId = []byte(test.content_id)
-      pssh.KeyId, err = base64.StdEncoding.DecodeString(test.key_id)
-      if err != nil {
-         t.Fatal(err)
-      }
-      var module widevine.Cdm
-      err = module.New(private_key, client_id, pssh.Marshal())
-      if err != nil {
-         t.Fatal(err)
-      }
-      data, err := module.RequestBody()
-      if err != nil {
-         t.Fatal(err)
-      }
-      data, err = session.Wrap(data)
-      if err != nil {
-         t.Fatal(err)
-      }
-      var body widevine.ResponseBody
-      err = body.Unmarshal(data)
-      if err != nil {
-         t.Fatal(err)
-      }
-      block, err := module.Block(body)
-      if err != nil {
-         t.Fatal(err)
-      }
-      containers := body.Container()
-      for {
-         container, ok := containers()
-         if !ok {
-            break
-         }
-         if bytes.Equal(container.Id(), pssh.KeyId) {
-            fmt.Printf("%x\n", container.Decrypt(block))
-         }
-      }
-      time.Sleep(time.Second)
-   }
-}
-
 var tests = []struct{
    content_id string
    location string
@@ -101,16 +36,16 @@ var tests = []struct{
       url: "cbs.com/shows/video/rZ59lcp4i2fU4dAaZJ_iEgKqVg_ogrIf",
    },
    {
+      content_id: "ssc3CuuS4mrQ7EyVXILH0FEQSi5yBAsA",
+      key_id: "dGO/CGkjTsqRU2Kz7vIp3g==",
+      location: "GB",
+      url: "paramountplus.com/shows/video/ssc3CuuS4mrQ7EyVXILH0FEQSi5yBAsA",
+   },
+   {
       content_id: "Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ",
       key_id: "BsO37qHORXefruKryNAaVQ==",
       location: "GB",
       url: "paramountplus.com/movies/video/Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ",
-   },
-   {
-      content_id: "ssc3CuuS4mrQ7EyVXILH0FEQSi5yBAsA",
-      key_id: "",
-      location: "GB",
-      url: "paramountplus.com/shows/video/ssc3CuuS4mrQ7EyVXILH0FEQSi5yBAsA",
    },
    {
       content_id: "WNujiS5PHkY5wN9doNY6MSo_7G8uBUcX",
@@ -186,5 +121,69 @@ func TestItemUsa(t *testing.T) {
          fmt.Printf("%q\n", text.Name(&item))
          time.Sleep(time.Second)
       }
+   }
+}
+func TestWidevine(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var app AppToken
+   err = app.ComCbsApp()
+   if err != nil {
+      t.Fatal(err)
+   }
+   for _, test := range tests {
+      session, err := app.Session(test.content_id)
+      if err != nil {
+         t.Fatal(err)
+      }
+      var pssh widevine.PsshData
+      pssh.ContentId = []byte(test.content_id)
+      pssh.KeyId, err = base64.StdEncoding.DecodeString(test.key_id)
+      if err != nil {
+         t.Fatal(err)
+      }
+      var module widevine.Cdm
+      err = module.New(private_key, client_id, pssh.Marshal())
+      if err != nil {
+         t.Fatal(err)
+      }
+      data, err := module.RequestBody()
+      if err != nil {
+         t.Fatal(err)
+      }
+      data, err = session.Wrap(data)
+      if err != nil {
+         t.Fatal(err)
+      }
+      var body widevine.ResponseBody
+      err = body.Unmarshal(data)
+      if err != nil {
+         t.Fatal(err)
+      }
+      block, err := module.Block(body)
+      if err != nil {
+         t.Fatal(err)
+      }
+      containers := body.Container()
+      for {
+         container, ok := containers()
+         if !ok {
+            break
+         }
+         if bytes.Equal(container.Id(), pssh.KeyId) {
+            fmt.Printf("%x\n", container.Decrypt(block))
+         }
+      }
+      time.Sleep(time.Second)
    }
 }
