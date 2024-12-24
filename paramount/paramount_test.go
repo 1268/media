69e35298d914b73056ad3e061843085a2eb12ac6
3052 <spen+3052@protonmail.com>
2024-12-24 14:13:29 -0600

    +func TestWrap(t *testing.T) {

--- paramount/paramount_test.go
+++ paramount/paramount_test.go
@@ -11,72 +11,6 @@ import (
    "time"
 )
 
-func TestWrap(t *testing.T) {
-   home, err := os.UserHomeDir()
-   if err != nil {
-      t.Fatal(err)
-   }
-   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
-   if err != nil {
-      t.Fatal(err)
-   }
-   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
-   if err != nil {
-      t.Fatal(err)
-   }
-   var app AppToken
-   err = app.ComCbsApp()
-   if err != nil {
-      t.Fatal(err)
-   }
-   for _, test := range tests {
-      session, err := app.Session(test.content_id)
-      if err != nil {
-         t.Fatal(err)
-      }
-      key_id, err := base64.StdEncoding.DecodeString(test.key_id)
-      if err != nil {
-         t.Fatal(err)
-      }
-      var pssh widevine.PsshData
-      pssh.KeyIds = [][]byte{key_id}
-      pssh.ContentId = []byte(test.content_id)
-      var module widevine.Cdm
-      err = module.New(private_key, client_id, pssh.Marshal())
-      if err != nil {
-         t.Fatal(err)
-      }
-      data, err := module.RequestBody()
-      if err != nil {
-         t.Fatal(err)
-      }
-      data, err = session.Wrap(data)
-      if err != nil {
-         t.Fatal(err)
-      }
-      var body widevine.ResponseBody
-      err = body.Unmarshal(data)
-      if err != nil {
-         t.Fatal(err)
-      }
-      block, err := module.Block(body)
-      if err != nil {
-         t.Fatal(err)
-      }
-      containers := body.Container()
-      for {
-         container, ok := containers()
-         if !ok {
-            break
-         }
-         if bytes.Equal(container.Id(), key_id) {
-            fmt.Printf("%x\n", container.Key(block))
-         }
-      }
-      time.Sleep(time.Second)
-   }
-}
-
 var tests = []struct{
    content_id string
    location string
@@ -187,5 +121,70 @@ func TestItemUsa(t *testing.T) {
          fmt.Printf("%q\n", text.Name(&item))
          time.Sleep(time.Second)
       }
+   }
+}
+func TestWrap(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var app AppToken
+   err = app.ComCbsApp()
+   if err != nil {
+      t.Fatal(err)
+   }
+   for _, test := range tests {
+      session, err := app.Session(test.content_id)
+      if err != nil {
+         t.Fatal(err)
+      }
+      key_id, err := base64.StdEncoding.DecodeString(test.key_id)
+      if err != nil {
+         t.Fatal(err)
+      }
+      var pssh widevine.PsshData
+      pssh.KeyIds = [][]byte{key_id}
+      pssh.ContentId = []byte(test.content_id)
+      var module widevine.Cdm
+      err = module.New(private_key, client_id, pssh.Marshal())
+      if err != nil {
+         t.Fatal(err)
+      }
+      data, err := module.RequestBody()
+      if err != nil {
+         t.Fatal(err)
+      }
+      data, err = session.Wrap(data)
+      if err != nil {
+         t.Fatal(err)
+      }
+      var body widevine.ResponseBody
+      err = body.Unmarshal(data)
+      if err != nil {
+         t.Fatal(err)
+      }
+      block, err := module.Block(body)
+      if err != nil {
+         t.Fatal(err)
+      }
+      containers := body.Container()
+      for {
+         container, ok := containers()
+         if !ok {
+            break
+         }
+         if bytes.Equal(container.Id(), key_id) {
+            fmt.Printf("%x\n", container.Key(block))
+         }
+      }
+      time.Sleep(time.Second)
    }
 }
