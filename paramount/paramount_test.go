fc6416ce7126e8d7d11a136910f6cb2377f7ec6a
3052 <spen+3052@protonmail.com>
2024-12-25 08:50:44 -0600

    +      content_id: "N5ySQTDzhLW2YyWGWuZvCb_wGsCQ_jCJ",

--- paramount/paramount_test.go
+++ paramount/paramount_test.go
@@ -42,10 +42,10 @@ var tests = []struct{
       url: "paramountplus.com/movies/video/Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ",
    },
    {
-      content_id: "ssc3CuuS4mrQ7EyVXILH0FEQSi5yBAsA",
-      key_id: "dGO/CGkjTsqRU2Kz7vIp3g==",
       location: "GB",
-      url: "paramountplus.com/shows/video/ssc3CuuS4mrQ7EyVXILH0FEQSi5yBAsA",
+      url: "paramountplus.com/shows/video/N5ySQTDzhLW2YyWGWuZvCb_wGsCQ_jCJ",
+      content_id: "N5ySQTDzhLW2YyWGWuZvCb_wGsCQ_jCJ",
+      key_id: "",
    },
    {
       content_id: "WNujiS5PHkY5wN9doNY6MSo_7G8uBUcX",
