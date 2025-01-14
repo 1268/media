6a6214bc3bb92da068dbb6d6e64a0a5a476b1fed
3052 <spen+3052@protonmail.com>
2025-01-14 17:32:29 -0600

    new file:   blog/rakuten/stream.go

--- blog/rakuten/rakuten.go
+++ blog/rakuten/rakuten.go
@@ -1,7 +1,6 @@
 package rakuten
 
 import (
-   "bytes"
    "encoding/json"
    "errors"
    "net/http"
@@ -10,69 +9,74 @@ import (
    "strings"
 )
 
-type on_demand struct {
-   AudioLanguage            string `json:"audio_language"`
-   AudioQuality             string `json:"audio_quality"`
-   ClassificationId         string `json:"classification_id"`
-   ContentId                string `json:"content_id"`
-   ContentType              string `json:"content_type"`
-   DeviceIdentifier         string `json:"device_identifier"`
-   DeviceSerial             string `json:"device_serial"`
-   DeviceStreamVideoQuality string `json:"device_stream_video_quality"`
-   Player                   string `json:"player"`
-   SubtitleLanguage         string `json:"subtitle_language"`
-   VideoType                string `json:"video_type"`
+type gizmo_season struct {
+   Episodes []gizmo_content
 }
 
-// geo block
-func (o *on_demand) streamings() ([]stream_info, error) {
-   o.AudioQuality = "2.0"
-   o.DeviceIdentifier = "atvui40"
-   o.DeviceStreamVideoQuality = "FHD"
-   o.Player = "atvui40:DASH-CENC:WVM"
-   o.SubtitleLanguage = "MIS"
-   o.VideoType = "stream"
-   o.DeviceSerial = "not implemented"
-   
-   o.ContentType = "movies"
-   o.AudioLanguage = "ENG"
-   
-   data, err := json.Marshal(o)
-   if err != nil {
-      return nil, err
-   }
-   resp, err := http.Post(
-      "https://gizmo.rakuten.tv/v3/avod/streamings",
-      "application/json", bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   var value struct {
-      Data struct {
-         StreamInfos []stream_info `json:"stream_infos"`
-      }
-      Errors []struct {
-         Message string
-      }
-   }
-   err = json.NewDecoder(resp.Body).Decode(&value)
-   if err != nil {
-      return nil, err
-   }
-   if err := value.Errors; len(err) >= 1 {
-      return nil, errors.New(err[0].Message)
-   }
-   return value.Data.StreamInfos, nil
+type address struct {
+   market_code string
+   season_id   string
+   content_id  string
 }
 
-func (g gizmo_season) content(web *address) (*gizmo_content, bool) {
-   for _, episode := range g.Episodes {
-      if episode.Id == web.content_id {
-         return &episode, true
+func (a *address) Set(data string) error {
+   data = strings.TrimPrefix(data, "https://")
+   data = strings.TrimPrefix(data, "www.")
+   data = strings.TrimPrefix(data, "rakuten.tv")
+   data = strings.TrimPrefix(data, "/")
+   var found bool
+   a.market_code, data, found = strings.Cut(data, "/")
+   if !found {
+      return errors.New("market code not found")
+   }
+   data, a.content_id, found = strings.Cut(data, "movies/")
+   if !found {
+      data = strings.TrimPrefix(data, "player/episodes/stream/")
+      a.season_id, a.content_id, found = strings.Cut(data, "/")
+      if !found {
+         return errors.New("episode not found")
       }
    }
-   return nil, false
+   return nil
+}
+
+type stream_info struct {
+   LicenseUrl   string `json:"license_url"`
+   Url          string
+   VideoQuality string `json:"video_quality"`
+}
+
+func (a *address) classification_id() (string, error) {
+   var v int
+   switch a.market_code {
+   case "cz":
+      v = 272
+   case "dk":
+      v = 283
+   case "fi":
+      v = 284
+   case "fr":
+      v = 23
+   case "ie":
+      v = 41
+   case "it":
+      v = 36
+   case "nl":
+      v = 323
+   case "no":
+      v = 286
+   case "pt":
+      v = 64
+   case "se":
+      v = 282
+   case "ua":
+      v = 276
+   case "uk":
+      v = 18
+   default:
+      return "", errors.New(a.market_code)
+   }
+   return strconv.Itoa(v), nil
 }
 
 func (a *address) season() (*gizmo_season, error) {
@@ -110,68 +114,31 @@ func (a *address) season() (*gizmo_season, error) {
    return &value.Data, nil
 }
 
-type gizmo_season struct {
-   Episodes []gizmo_content
-}
-
-type address struct {
-   market_code string
-   season_id   string
-   content_id  string
-}
-
-func (a *address) Set(data string) error {
-   data = strings.TrimPrefix(data, "https://")
-   data = strings.TrimPrefix(data, "www.")
-   data = strings.TrimPrefix(data, "rakuten.tv")
-   data = strings.TrimPrefix(data, "/")
-   var found bool
-   a.market_code, data, found = strings.Cut(data, "/")
-   if !found {
-      return errors.New("market code not found")
-   }
-   data, a.content_id, found = strings.Cut(data, "movies/")
-   if !found {
-      data = strings.TrimPrefix(data, "player/episodes/stream/")
-      a.season_id, a.content_id, found = strings.Cut(data, "/")
-      if !found {
-         return errors.New("episode not found")
+func (a *address) content(season gizmo_season) (*gizmo_content, bool) {
+   for _, episode := range season.Episodes {
+      if episode.Id == a.content_id {
+         return &episode, true
       }
    }
-   return nil
+   return nil, false
 }
 
-func (a *address) classification_id() (string, error) {
-   var v int
-   switch a.market_code {
-   case "cz":
-      v = 272
-   case "dk":
-      v = 283
-   case "fi":
-      v = 284
-   case "fr":
-      v = 23
-   case "ie":
-      v = 41
-   case "it":
-      v = 36
-   case "nl":
-      v = 323
-   case "no":
-      v = 286
-   case "pt":
-      v = 64
-   case "se":
-      v = 282
-   case "ua":
-      v = 276
-   case "uk":
-      v = 18
-   default:
-      return "", errors.New(a.market_code)
-   }
-   return strconv.Itoa(v), nil
+type gizmo_content struct {
+   Number       int
+   SeasonNumber int `json:"season_number"`
+   Title        string
+   TvShowTitle  string `json:"tv_show_title"`
+   ViewOptions  struct {
+      Private struct {
+         Streams []struct {
+            AudioLanguages []struct {
+               Id string
+            } `json:"audio_languages"`
+         }
+      }
+   } `json:"view_options"`
+   Year int
+   Id   string
 }
 
 func (a *address) movie() (*gizmo_content, error) {
@@ -207,28 +174,4 @@ func (a *address) movie() (*gizmo_content, error) {
       return nil, err
    }
    return &value.Data, nil
-}
-
-type stream_info struct {
-   LicenseUrl   string `json:"license_url"`
-   Url          string
-   VideoQuality string `json:"video_quality"`
-}
-
-type gizmo_content struct {
-   Id           string
-   Number       int
-   SeasonNumber int `json:"season_number"`
-   Title        string
-   TvShowTitle  string `json:"tv_show_title"`
-   ViewOptions  struct {
-      Private struct {
-         Streams []struct {
-            AudioLanguages []struct {
-               Id string
-            } `json:"audio_languages"`
-         }
-      }
-   } `json:"view_options"`
-   Year int
 }
---
+++ blog/rakuten/stream.go
@@ -0,0 +1,55 @@
+package rakuten
+
+import (
+   "bytes"
+   "encoding/json"
+   "errors"
+   "net/http"
+)
+
+// geo block
+func (a *address) streamings(content *gizmo_content) ([]stream_info, error) {
+   classification, err := a.classification_id()
+   if err != nil {
+      return nil, err
+   }
+   data, err := json.Marshal(map[string]string{
+      "audio_language":              "ENG",
+      "audio_quality":               "2.0",
+      "classification_id":           classification,
+      "content_id":                  content.Id,
+      "content_type":                "movies",
+      "device_identifier":           "atvui40",
+      "device_serial":               "not implemented",
+      "device_stream_video_quality": "FHD",
+      "player":                      "atvui40:DASH-CENC:WVM",
+      "subtitle_language":           "MIS",
+      "video_type":                  "stream",
+   })
+   if err != nil {
+      return nil, err
+   }
+   resp, err := http.Post(
+      "https://gizmo.rakuten.tv/v3/avod/streamings",
+      "application/json", bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   var value struct {
+      Data struct {
+         StreamInfos []stream_info `json:"stream_infos"`
+      }
+      Errors []struct {
+         Message string
+      }
+   }
+   err = json.NewDecoder(resp.Body).Decode(&value)
+   if err != nil {
+      return nil, err
+   }
+   if err := value.Errors; len(err) >= 1 {
+      return nil, errors.New(err[0].Message)
+   }
+   return value.Data.StreamInfos, nil
+}
