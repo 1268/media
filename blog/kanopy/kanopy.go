dd719e4a3a2ab24f37b6e75f7a94fc6acdf327db
3052 <spen+3052@protonmail.com>
2025-01-22 21:07:06 -0600

    +func (w *web_token) membership() (*membership, error) {

--- blog/kanopy/kanopy.go
+++ blog/kanopy/kanopy.go
@@ -9,41 +9,6 @@ import (
    "strconv"
 )
 
-func (w *web_token) membership() (*membership, error) {
-   req, err := http.NewRequest("", "https://www.kanopy.com", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/kapi/memberships"
-   req.URL.RawQuery = "userId=" + strconv.FormatInt(w.UserId, 10)
-   req.Header = http.Header{
-      "authorization": {"Bearer " + w.Jwt},
-      "user-agent": {user_agent},
-      "x-version": {x_version},
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var member struct {
-      List []membership
-   }
-   err = json.NewDecoder(resp.Body).Decode(&member)
-   if err != nil {
-      return nil, err
-   }
-   return &member.List[0], nil
-}
-
-type membership struct {
-   DomainId int64
-}
-
-type video_plays struct {
-   Manifests []video_manifest
-}
-
 func (w *web_token) plays(
    member *membership, video_id int64,
 ) (*video_plays, error) {
@@ -81,6 +46,41 @@ func (w *web_token) plays(
       return nil, err
    }
    return play, nil
+}
+
+func (w *web_token) membership() (*membership, error) {
+   req, err := http.NewRequest("", "https://www.kanopy.com", nil)
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Path = "/kapi/memberships"
+   req.URL.RawQuery = "userId=" + strconv.FormatInt(w.UserId, 10)
+   req.Header = http.Header{
+      "authorization": {"Bearer " + w.Jwt},
+      "user-agent": {user_agent},
+      "x-version": {x_version},
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var member struct {
+      List []membership
+   }
+   err = json.NewDecoder(resp.Body).Decode(&member)
+   if err != nil {
+      return nil, err
+   }
+   return &member.List[0], nil
+}
+
+type membership struct {
+   DomainId int64
+}
+
+type video_plays struct {
+   Manifests []video_manifest
 }
 
 type wrapper struct {
@@ -162,87 +162,6 @@ func (w *wrapper) Wrap(data []byte) ([]byte, error) {
    }
    defer resp.Body.Close()
    return io.ReadAll(resp.Body)
-}
-
-///
-
-func (v video_items) item(id int64) (int, *video_item) {
-   for key, value := range v.List {
-      if value.Video != nil {
-         if value.Video.VideoId == id {
-            return key, &value
-         }
-      } else {
-         if value.Playlist.VideoId == id {
-            return key, &value
-         }
-      }
-   }
-   return -1, nil
-}
-
-type video_items struct {
-   List []video_item
-}
-
-type video_item struct {
-   Playlist *struct {
-      Title string
-      VideoId int64
-   }
-   Video *struct {
-      Title string
-      VideoId int64
-   }
-}
-
-func (v *video_item) String() string {
-   var b []byte
-   if v.Video != nil {
-      b = append(b, "title = "...)
-      b = append(b, v.Video.Title...)
-      b = append(b, "\nvideo id = "...)
-      b = strconv.AppendInt(b, v.Video.VideoId, 10)
-   } else {
-      b = append(b, "title = "...)
-      b = append(b, v.Playlist.Title...)
-      b = append(b, "\nvideo id = "...)
-      b = strconv.AppendInt(b, v.Playlist.VideoId, 10)
-   }
-   return string(b)
-}
-
-type video_response struct {
-   AncestorVideoIds []int64
-   ProductionYear int
-   Title string
-   VideoId int64
-}
-
-func (w *web_token) video(id int64) (*video_response, error) {
-   req, err := http.NewRequest("", "https://www.kanopy.com", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/kapi/videos/" + strconv.FormatInt(id, 10)
-   req.Header = http.Header{
-      "authorization": {"Bearer " + w.Jwt},
-      "user-agent": {user_agent},
-      "x-version": {x_version},
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var video struct {
-      Video video_response
-   }
-   err = json.NewDecoder(resp.Body).Decode(&video)
-   if err != nil {
-      return nil, err
-   }
-   return &video.Video, nil
 }
 
 const x_version = "!/!/!/!"
