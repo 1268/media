2d86791bb5bdc5cc9fb6084adee3ff684baecdab
3052 <spen+3052@protonmail.com>
2025-02-01 15:18:33 -0600

    +func (p *login_page) login_page(

--- blog/sky/tv/login.go
+++ blog/sky/tv/login.go
@@ -8,27 +8,119 @@ import (
    "strings"
 )
 
-func (p *login_page) login_page() (*http.Response, error) {
-   data = {
-      'username': __login, 'password': __password,
-      'returnUrl': 'Home/HomeTv', 'subscriptionUrl': '/de/subscription',
-      cookie_check["rvtp"]: page_token,
+func (p *login_page) login_page(
+   username, password string,
+) (*http.Response, error) {
+   page_token, err := p.page_token()
+   if err != nil {
+      return nil, err
    }
-   return requests.post(
-      auth_url, timeout=5, headers=login_headers,
-      cookies=cookies,
-      data=data,
-      allow_redirects=False, verify=False
+   data := url.Values{
+      "__RequestVerificationToken": {page_token},
+      "password": {password},
+      "returnUrl": {"Home/HomeTv"},
+      "subscriptionUrl": {"/de/subscription"},
+      "username": {username},
+   }.Encode()
+   req, err := http.NewRequest(
+      "POST", "https://show.sky.ch/de/Authentication/Login",
+      strings.NewReader(data),
    )
+   req.Header.Set("accept-language", "de")
+   req.Header.Set("referer", "https://show.sky.ch/de/tv/")
+   req.Header.Set("tv", "Emulator")
+   req.Header.Set(
+      "user-agent",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0",
+   )
+   cookie_token, err := p.cookie_token()
+   if err != nil {
+      return nil, err
+   }
+   req.AddCookie(cookie_token)
+   asp_cookie, err := p.asp_cookie()
+   if err != nil {
+      return nil, err
+   }
+   req.AddCookie(asp_cookie)
+   req.AddCookie(p.sky_tv_device())
+   return http.DefaultClient.Do(req)
 }
 
-func (p *login_page) page_token() (string, bool) {
-   for _, input := range p.section.Div.Form.Input {
-      if input.Name == "__RequestVerificationToken" {
-         return input.Value, true
+func (*login_page) sky_tv_device() *http.Cookie {
+   var cookie http.Cookie
+   cookie.Name = "SkyTvDevice"
+   cookie.Value = url.QueryEscape(`
+   {
+      "isSky": true,
+      "keys": {
+         "back": 461,
+         "down": 40,
+         "enter": 13,
+         "ff": 417,
+         "ff10": -1,
+         "key0": -1,
+         "key1": -1,
+         "key2": -1,
+         "key3": -1,
+         "key4": -1,
+         "key5": -1,
+         "key6": -1,
+         "key7": -1,
+         "key8": -1,
+         "key9": -1,
+         "left": 37,
+         "pause": 19,
+         "play": 415,
+         "playPause": -1,
+         "rew": 412,
+         "rew10": -1,
+         "right": 39,
+         "search": -1,
+         "stop": 413,
+         "up": 38
+      },
+      "type": {
+         "code": "Desktop"
+      },
+      "year": ""
+   }
+   `)
+   return &cookie
+}
+
+func (p login_page) asp_cookie() (*http.Cookie, error) {
+   for _, cookie := range p.cookies {
+      if cookie.Name == "_ASP.NET_SessionId_" {
+         return cookie, nil
       }
    }
-   return "", false
+   return nil, http.ErrNoCookie
+}
+
+func (p login_page) cookie_token() (*http.Cookie, error) {
+   for _, cookie := range p.cookies {
+      if cookie.Name == "__RequestVerificationToken" {
+         return cookie, nil
+      }
+   }
+   return nil, http.ErrNoCookie
+}
+
+var login_headers = http.Header{
+   "accept-language": {"de"},
+   "referer": {"https://show.sky.ch/de/tv/"},
+   "tv": {"Emulator"},
+   "user-agent": {"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0"},
+}
+
+func (p *login_page) page_token() (string, error) {
+   for _, input := range p.section.Div.Form.Input {
+      if input.Name == "__RequestVerificationToken" {
+         return input.Value, nil
+      }
+   }
+   return "", http.ErrNoCookie
 }
 
 var cookie_check = map[string]string{
@@ -74,72 +166,3 @@ func (p *login_page) New() error {
 }
 
 const out_of_country = "/out-of-country"
-
-var login_headers = http.Header{
-   "accept-language": {"de"},
-   "referer": {"https://show.sky.ch/de/tv/"},
-   "tv": {"Emulator"},
-   "user-agent": {"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0"},
-}
-
-const auth_url = "https://show.sky.ch/de/Authentication/Login"
-
-func (p login_page) asp_cookie() (*http.Cookie, bool) {
-   for _, cookie := range p.cookies {
-      if cookie.Name == "_ASP.NET_SessionId_" {
-         return cookie, true
-      }
-   }
-   return nil, false
-}
-
-func (p login_page) cookie_token() (*http.Cookie, bool) {
-   for _, cookie := range p.cookies {
-      if cookie.Name == "__RequestVerificationToken" {
-         return cookie, true
-      }
-   }
-   return nil, false
-}
-
-func (*login_page) sky_tv_device() http.Cookie {
-   var cookie http.Cookie
-   cookie.Name = "SkyTvDevice"
-   cookie.Value = url.QueryEscape(`
-   {
-      "isSky": true,
-      "keys": {
-         "back": 461,
-         "down": 40,
-         "enter": 13,
-         "ff": 417,
-         "ff10": -1,
-         "key0": -1,
-         "key1": -1,
-         "key2": -1,
-         "key3": -1,
-         "key4": -1,
-         "key5": -1,
-         "key6": -1,
-         "key7": -1,
-         "key8": -1,
-         "key9": -1,
-         "left": 37,
-         "pause": 19,
-         "play": 415,
-         "playPause": -1,
-         "rew": 412,
-         "rew10": -1,
-         "right": 39,
-         "search": -1,
-         "stop": 413,
-         "up": 38
-      },
-      "type": {
-         "code": "Desktop"
-      },
-      "year": ""
-   }
-   `)
-   return cookie
-}
