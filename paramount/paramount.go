021b8809e2f5215474d58a3c5a3fbbe6c0a099cc
10317 <10317@proton.me>
2025-02-15 00:01:34 -0600

    +func pad(data []byte) []byte {

--- paramount/paramount.go
+++ paramount/paramount.go
@@ -16,12 +16,49 @@ import (
    "time"
 )
 
-// must use app token and IP address for correct location
-func (VideoItem) Marshal(app *AppToken, cid string) ([]byte, error) {
-   req, err := http.NewRequest("", "https://www.paramountplus.com", nil)
-   if err != nil {
-      return nil, err
+const secret_key = "302a6a0d70a7e9b967f91d39fef3e387816e3095925ae4537bce96063311f9c5"
+
+func pad(data []byte) []byte {
+   length := aes.BlockSize - len(data) % aes.BlockSize
+   for high := byte(length); length >= 1; length-- {
+      data = append(data, high)
    }
+   return data
+}
+
+const encoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+func cms_account(id string) int64 {
+   var (
+      i = 0
+      j = 1
+   )
+   for _, value := range id {
+      i += strings.IndexRune(encoding, value) * j
+      j *= len(encoding)
+   }
+   return int64(i)
+}
+
+type AppToken struct {
+   AppSecret string
+   SecretKey string
+}
+
+type SessionToken struct {
+   LsSession string `json:"ls_session"`
+   Url string
+}
+
+type Item struct {
+   AssetType string
+   CmsAccountId string
+   ContentId string
+}
+
+// must use app token and IP address for correct location
+func (Item) Marshal(token *AppToken, cid string) ([]byte, error) {
+   req, _ := http.NewRequest("", "https://www.paramountplus.com", nil)
    req.URL.Path = func() string {
       var b strings.Builder
       b.WriteString("/apps-api/v2.0/androidphone/video/cid/")
@@ -29,28 +66,28 @@ func (VideoItem) Marshal(app *AppToken, cid string) ([]byte, error) {
       b.WriteString(".json")
       return b.String()
    }()
-   token, err := app.encode()
+   token1, err := token.encode()
    if err != nil {
       return nil, err
    }
-   req.URL.RawQuery = url.Values{"at": {token}}.Encode()
+   req.URL.RawQuery = url.Values{"at": {token1}}.Encode()
    resp, err := http.DefaultClient.Do(req)
    if err != nil {
       return nil, err
    }
    defer resp.Body.Close()
    if resp.StatusCode != http.StatusOK {
-      var b strings.Builder
-      resp.Write(&b)
-      return nil, errors.New(b.String())
+      var data strings.Builder
+      resp.Write(&data)
+      return nil, errors.New(data.String())
    }
    return io.ReadAll(resp.Body)
 }
 
-func (v *VideoItem) Unmarshal(data []byte) error {
+func (i *Item) Unmarshal(data []byte) error {
    var value struct {
       Error string
-      ItemList []VideoItem
+      ItemList []Item
    }
    err := json.Unmarshal(data, &value)
    if err != nil {
@@ -62,87 +99,22 @@ func (v *VideoItem) Unmarshal(data []byte) error {
    if len(value.ItemList) == 0 {
       return errors.New(`"itemList":[]`)
    }
-   *v = value.ItemList[0]
+   *i = value.ItemList[0]
    return nil
 }
 
 // hard geo block
-func (v *VideoItem) Mpd() string {
+func (i *Item) Mpd() string {
    b := []byte("https://link.theplatform.com/s/")
-   b = append(b, v.CmsAccountId...)
+   b = append(b, i.CmsAccountId...)
    b = append(b, "/media/guid/"...)
-   b = strconv.AppendInt(b, cms_account(v.CmsAccountId), 10)
+   b = strconv.AppendInt(b, cms_account(i.CmsAccountId), 10)
    b = append(b, '/')
-   b = append(b, v.ContentId...)
+   b = append(b, i.ContentId...)
    b = append(b, "?assetTypes="...)
-   b = append(b, v.AssetType...)
+   b = append(b, i.AssetType...)
    b = append(b, "&formats=MPEG-DASH"...)
    return string(b)
-}
-
-// must use app token and IP address for US
-func (a *AppToken) Session(content_id string) (*SessionToken, error) {
-   req, err := http.NewRequest("", "https://www.paramountplus.com", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = func() string {
-      var b strings.Builder
-      b.WriteString("/apps-api/v3.1/androidphone/irdeto-control")
-      b.WriteString("/anonymous-session-token.json")
-      return b.String()
-   }()
-   token, err := a.encode()
-   if err != nil {
-      return nil, err
-   }
-   req.URL.RawQuery = url.Values{
-      "at": {token},
-      "contentId": {content_id},
-   }.Encode()
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      var b strings.Builder
-      resp.Write(&b)
-      return nil, errors.New(b.String())
-   }
-   session := &SessionToken{}
-   err = json.NewDecoder(resp.Body).Decode(session)
-   if err != nil {
-      return nil, err
-   }
-   return session, nil
-}
-
-func (s *SessionToken) Wrap(data []byte) ([]byte, error) {
-   req, err := http.NewRequest("POST", s.Url, bytes.NewReader(data))
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "authorization": {"Bearer " + s.LsSession},
-      "content-type": {"application/x-protobuf"},
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   return io.ReadAll(resp.Body)
-}
-
-type SessionToken struct {
-   LsSession string `json:"ls_session"`
-   Url string
-}
-
-type AppToken struct {
-   AppSecret string
-   SecretKey string
 }
 
 // 15.0.52
@@ -166,47 +138,65 @@ func (a *AppToken) encode() (string, error) {
    if err != nil {
       return "", err
    }
-   src := []byte{'|'}
-   src = append(src, a.AppSecret...)
-   src = pad(src)
    var iv [aes.BlockSize]byte
-   cipher.NewCBCEncrypter(block, iv[:]).CryptBlocks(src, src)
-   var dst []byte
-   dst = append(dst, 0, aes.BlockSize)
-   dst = append(dst, iv[:]...)
-   dst = append(dst, src...)
-   return base64.StdEncoding.EncodeToString(dst), nil
+   data := []byte{'|'}
+   data = append(data, a.AppSecret...)
+   data = pad(data)
+   cipher.NewCBCEncrypter(block, iv[:]).CryptBlocks(data, data)
+   data1 := []byte{0, aes.BlockSize}
+   data1 = append(data1, iv[:]...)
+   data1 = append(data1, data...)
+   return base64.StdEncoding.EncodeToString(data1), nil
 }
 
-const secret_key = "302a6a0d70a7e9b967f91d39fef3e387816e3095925ae4537bce96063311f9c5"
-
-func pad(b []byte) []byte {
-   length := aes.BlockSize - len(b) % aes.BlockSize
-   for high := byte(length); length >= 1; length-- {
-      b = append(b, high)
+func (s *SessionToken) License(data []byte) ([]byte, error) {
+   req, err := http.NewRequest("POST", s.Url, bytes.NewReader(data))
+   if err != nil {
+      return nil, err
    }
-   return b
-}
-
-const encoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-
-func cms_account(id string) int64 {
-   var (
-      i = 0
-      j = 1
-   )
-   for _, value := range id {
-      i += strings.IndexRune(encoding, value) * j
-      j *= len(encoding)
+   req.Header = http.Header{
+      "authorization": {"Bearer " + s.LsSession},
+      "content-type": {"application/x-protobuf"},
    }
-   return int64(i)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   return io.ReadAll(resp.Body)
 }
 
-type VideoItem struct {
-   AirDateIso time.Time `json:"_airDateISO"`
-   AssetType string
-   CmsAccountId string
-   ContentId string
-   Label string
-   SeriesTitle string
+// must use app token and IP address for US
+func (a *AppToken) Session(content_id string) (*SessionToken, error) {
+   req, _ := http.NewRequest("", "https://www.paramountplus.com", nil)
+   req.URL.Path = func() string {
+      var b strings.Builder
+      b.WriteString("/apps-api/v3.1/androidphone/irdeto-control")
+      b.WriteString("/anonymous-session-token.json")
+      return b.String()
+   }()
+   token, err := a.encode()
+   if err != nil {
+      return nil, err
+   }
+   req.URL.RawQuery = url.Values{
+      "at": {token},
+      "contentId": {content_id},
+   }.Encode()
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      var data strings.Builder
+      resp.Write(&data)
+      return nil, errors.New(data.String())
+   }
+   session := &SessionToken{}
+   err = json.NewDecoder(resp.Body).Decode(session)
+   if err != nil {
+      return nil, err
+   }
+   return session, nil
 }
