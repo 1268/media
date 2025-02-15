edd03ef662677beaf0f60d091f4e0054bf5ef3ee
10317 <10317@proton.me>
2025-02-15 14:29:47 -0600

    +func (a Address) String() string {

--- ctv/ctv.go
+++ ctv/ctv.go
@@ -10,9 +10,61 @@ import (
    "strings"
 )
 
+func (a Address) String() string {
+   return a[0]
+}
+
+// https://www.ctv.ca/shows/friends/the-one-with-the-bullies-s2e21
+func (a *Address) Set(data string) error {
+   data = strings.TrimPrefix(data, "https://")
+   data = strings.TrimPrefix(data, "www.")
+   (*a)[0] = strings.TrimPrefix(data, "ctv.ca")
+   return nil
+}
+
+type Address [1]string
+
+type Client struct{}
+
+func (m Manifest) Mpd() (*http.Response, error) {
+   return http.Get(m[0])
+}
+
+func (m *Manifest) Unmarshal(data []byte) error {
+   (*m)[0] = strings.Replace(string(data), "/best/", "/ultimate/", 1)
+   return nil
+}
+
+type Manifest [1]string
+
+type AxisContent struct {
+   AxisId                int64
+   AxisPlaybackLanguages []struct {
+      DestinationCode string
+   }
+}
+
+type Content struct {
+   ContentPackages []struct {
+      Id int64
+   }
+}
+
+type ResolvedPath struct {
+   LastSegment struct {
+      Content struct {
+         FirstPlayableContent *struct {
+            Id string
+         }
+         Id                   string
+      }
+   }
+}
+
 func (r *ResolvedPath) get_id() string {
-   if first := r.LastSegment.Content.FirstPlayableContent; first != nil {
-      return first.Id
+   content1 := r.LastSegment.Content.FirstPlayableContent
+   if content1 != nil {
+      return c.Id
    }
    return r.LastSegment.Content.Id
 }
@@ -59,53 +111,6 @@ func (r *ResolvedPath) Axis() (*AxisContent, error) {
    return &value.Data.AxisContent, nil
 }
 
-func (m Manifest) Mpd() (*http.Response, error) {
-   return http.Get(m.S)
-}
-
-// hard geo block
-func (Manifest) Marshal(axis *AxisContent, content0 *Content) ([]byte, error) {
-   req, _ := http.NewRequest("", "https://capi.9c9media.com", nil)
-   req.URL.Path = func() string {
-      b := []byte("/destinations/")
-      b = append(b, axis.AxisPlaybackLanguages[0].DestinationCode...)
-      b = append(b, "/platforms/desktop/playback/contents/"...)
-      b = strconv.AppendInt(b, axis.AxisId, 10)
-      b = append(b, "/contentPackages/"...)
-      b = strconv.AppendInt(b, content0.ContentPackages[0].Id, 10)
-      b = append(b, "/manifest.mpd"...)
-      return string(b)
-   }()
-   req.URL.RawQuery = "action=reference"
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      var data strings.Builder
-      resp.Write(&data)
-      return nil, errors.New(data.String())
-   }
-   return io.ReadAll(resp.Body)
-}
-
-func (m *Manifest) Unmarshal(data []byte) error {
-   m.S = strings.Replace(string(data), "/best/", "/ultimate/", 1)
-   return nil
-}
-func (a *Address) String() string {
-   return a.s
-}
-
-// https://www.ctv.ca/shows/friends/the-one-with-the-bullies-s2e21
-func (a *Address) Set(data string) error {
-   a.s = strings.TrimPrefix(data, "https://")
-   a.s = strings.TrimPrefix(a.s, "www.")
-   a.s = strings.TrimPrefix(a.s, "ctv.ca")
-   return nil
-}
-
 // YOU CANNOT USE ANONYMOUS QUERY!
 const query_axis = `
 query axisContent($id: ID!) {
@@ -143,11 +148,74 @@ query resolvePath($path: String!) {
    }
 }
 `
+
+func (Client) License(data []byte) ([]byte, error) {
+   resp, err := http.Post(
+      "https://license.9c9media.ca/widevine", "application/x-protobuf",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   return io.ReadAll(resp.Body)
+}
+
+// hard geo block
+func (Manifest) Marshal(axis *AxisContent, content1 *Content) ([]byte, error) {
+   req, _ := http.NewRequest("", "https://capi.9c9media.com", nil)
+   req.URL.Path = func() string {
+      b := []byte("/destinations/")
+      b = append(b, axis.AxisPlaybackLanguages[0].DestinationCode...)
+      b = append(b, "/platforms/desktop/playback/contents/"...)
+      b = strconv.AppendInt(b, axis.AxisId, 10)
+      b = append(b, "/contentPackages/"...)
+      b = strconv.AppendInt(b, content1.ContentPackages[0].Id, 10)
+      b = append(b, "/manifest.mpd"...)
+      return string(b)
+   }()
+   req.URL.RawQuery = "action=reference"
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      var data strings.Builder
+      resp.Write(&data)
+      return nil, errors.New(data.String())
+   }
+   return io.ReadAll(resp.Body)
+}
+
+func (a *AxisContent) Content() (*Content, error) {
+   req, _ := http.NewRequest("", "https://capi.9c9media.com", nil)
+   req.URL.Path = func() string {
+      b := []byte("/destinations/")
+      b = append(b, a.AxisPlaybackLanguages[0].DestinationCode...)
+      b = append(b, "/platforms/desktop/contents/"...)
+      b = strconv.AppendInt(b, a.AxisId, 10)
+      return string(b)
+   }()
+   req.URL.RawQuery = "$include=[ContentPackages]"
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   content1 := &Content{}
+   err = json.NewDecoder(resp.Body).Decode(content1)
+   if err != nil {
+      return nil, err
+   }
+   return content1, nil
+}
+
 func (a Address) Resolve() (*ResolvedPath, error) {
    data, err := json.Marshal(map[string]any{
       "query": graphql_compact(query_resolve),
       "variables": map[string]string{
-         "path": a.s,
+         "path": a[0]
       },
    })
    if err != nil {
@@ -184,79 +252,4 @@ func (a Address) Resolve() (*ResolvedPath, error) {
       return nil, errors.New(string(data))
    }
    return value.Data.ResolvedPath, nil
-}
-
-func (a *AxisContent) Content() (*Content, error) {
-   req, _ := http.NewRequest("", "https://capi.9c9media.com", nil)
-   req.URL.Path = func() string {
-      b := []byte("/destinations/")
-      b = append(b, a.AxisPlaybackLanguages[0].DestinationCode...)
-      b = append(b, "/platforms/desktop/contents/"...)
-      b = strconv.AppendInt(b, a.AxisId, 10)
-      return string(b)
-   }()
-   req.URL.RawQuery = "$include=[ContentPackages,Media,Season]"
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   content0 := &Content{}
-   err = json.NewDecoder(resp.Body).Decode(content0)
-   if err != nil {
-      return nil, err
-   }
-   return content0, nil
-}
-
-func (Client) License(data []byte) (*http.Response, error) {
-   return http.Post(
-      "https://license.9c9media.ca/widevine", "application/x-protobuf",
-      bytes.NewReader(data),
-   )
-}
-
-///
-
-type Address struct {
-   s string
-}
-
-type AxisContent struct {
-   AxisId                int64
-   AxisPlaybackLanguages []struct {
-      DestinationCode string
-   }
-}
-
-type Client struct{}
-
-type Content struct {
-   ContentPackages []struct {
-      Id int64
-   }
-   Episode int
-   Media   struct {
-      Name string
-      Type string
-   }
-   Name   string
-   Season struct {
-      Number int
-   }
-}
-
-type Manifest struct {
-   S string
-}
-
-type ResolvedPath struct {
-   LastSegment struct {
-      Content struct {
-         FirstPlayableContent *struct {
-            Id string
-         }
-         Id                   string
-      }
-   }
 }
